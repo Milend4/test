a72634a5ce8a08611c8c0220f1954168a5414938
Milend4 <milenaleticia.15sa@gmail.com>
2023-12-28 14:16:26 -0300

    up

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ from kivy.uix.image import Image
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.behaviors import ButtonBehavior
+from kivy.animation import Animation
+from kivy.base import runTouchApp
 import random
 
+JUMP_VEL= 8.5
 
 kv_string = '''
 <RunDinoGame>:
     canvas.before:
         Color:
-            rgba: 0.5, 0.5, 1, 1  # Cor do fundo (azul claro, ajuste conforme necessário)
+            rgba: 0.5, 0.5, 1, 1 
         Rectangle:
             pos: self.pos
             size: self.size
@@ -22,7 +25,7 @@ kv_string = '''
     Image:
         id: ground_image
         source: 'imagens/solo.png'
-        pos: root.ground_pos[0], root.ground_pos[1] + 210
+        pos: root.ground_pos[0], root.ground_pos[1] + 270
         size: self.texture_size
 
     Image:
@@ -30,12 +33,23 @@ kv_string = '''
         source: 'imagens/DinoRun1.png'
         pos: self.parent.center_x - self.width / 2-400, self.parent.center_y - self.height / 2-100
 
+    Button:
+        size_hint: None, None
+        size: 100, 82
+        pos: root.width - self.width - 80, 140
+        background_normal: 'imagens/seta.png'  
+        on_press: root.dino_jump()
 '''
+
 
 Builder.load_string(kv_string)
 
 # Configuração da janela
-Window.size = (800, 600)
+Window.size = (900, 680)
+
+class JumpArrow(Widget, ButtonBehavior):
+    pass
+
 
 class Cactus(Image):
     velocity_x = NumericProperty(0)
@@ -55,57 +69,23 @@ class Bird(Image):
     def move(self):
         self.x -= self.velocity_x
 
-
+class JumpArrow(Widget, ButtonBehavior):
+    pass
 # Classe do Dinossauro
 class Dino(Widget):
-    velocity_x = BoundedNumericProperty(0, min=-10, max=10)
-    velocity_y = BoundedNumericProperty(0, min=-10, max=10)
+    jump_image_paths = ['imagens/DinoJump.png']
+    velocity_x = 0
+    velocity_y = 0
     ground_pos = NumericProperty(0)
-    acceleration_y = NumericProperty(0)
-    jumping = NumericProperty(0)
     
-    def set_dino_image(self, image_path):
-        self.ids.dino_image.source = image_path
-
     def move(self):
         self.pos = Vector(self.velocity_x, self.velocity_y) + self.pos
 
         if self.pos[1] < self.ground_pos:
-            self.pos [1]= self.ground_pos
-            self.velocity_y < 0
-
-    def update(self, dt):
-        self.velocity_y += self.acceleration_y * dt
-        self.y += self.velocity_y * dt
-
-        if self.y < self.ground_pos:
-            self.y = self.ground_pos
+            self.pos[1] = self.ground_pos
             self.velocity_y = 0
-            self.jumping = 0
 
-    def jump(self, jump_height):
-        if self.jumping == 0:
-            self.jumping = 1
-            self.acceleration_y = 1000  # Ajuste conforme necessário para a altura do pulo
-            Clock.schedule_once(self.reset_jump, 0.2)
-
-    def reset_jump(self, dt):
-        self.acceleration_y = -1000
-    
-    def update_image(self):
-        if self.jumping:
-            self.ids.dino_image.source = 'imagens/DinoJump.png'
-        else:
-            self.ids.dino_image.source = self.image_paths[self.current_index]
-
-    def update_jump(self, dt):
-        self.move()
-        self.update_image()
-
-
-
-
-
+   
 # Classe do Jogo
 class RunDinoGame(Widget):
 
@@ -122,7 +102,6 @@ class RunDinoGame(Widget):
 
     current_dino_index = 0
     ground_speed = 4 #velocidade do solo
-    dino = ObjectProperty(None)
     ground_pos = ListProperty([0, 0])
     
 
@@ -137,23 +116,11 @@ class RunDinoGame(Widget):
         Clock.schedule_interval(self.update, 1.0 / 60.0)
         Clock.schedule_interval(self.change_dino_image, 0.175)        
         Clock.schedule_interval(self.spawn_clouds, 0.5) 
-        Clock.schedule_interval(self.spawn_obstacle, 1.5)
+        Clock.schedule_interval(self.spawn_obstacle, 2.0)
         Clock.schedule_interval(self.animate_birds, 0.2)  
-
-    def on_key_down(self, keyboard, keycode, text, modifiers):
-        if keycode[1] == 273:
-            self.dino.jump(jump_height=300)
-
-    def on_touch_down(self, touch):
-        if touch.y > self.height * 0.75:  
-            self.dino.jump(jump_height=300)
     
     def update(self, dt):
         self.dino.update(dt)
-
-    def on_touch_down(self, touch):
-        self.dino.jump(jump_height=300)
-
 
     def spawn_clouds(self, dt):
            if all(cloud.x < 0 for cloud in self.clouds):
@@ -172,7 +139,7 @@ class RunDinoGame(Widget):
 
     def adjust_obstacle_distance(self, obstacles, dt):
         # Ajusta a distância entre os obstáculos
-        min_distance_between_obstacles = 600  # Ajuste conforme necessário
+        min_distance_between_obstacles = 600 
         if len(obstacles) > 1:
             last_obstacle = obstacles[-2]
             if obstacles[-1].x - last_obstacle.x < min_distance_between_obstacles:
@@ -194,7 +161,7 @@ class RunDinoGame(Widget):
 
         while last_obstacle is not None and bird.x - last_obstacle.x < min_distance_between_obstacles:
             bird.x += 10
-        bird.pos = (self.width + random.randint(50, 150), self.ground_pos[1] + 310)
+        bird.pos = (self.width + random.randint(50, 150), self.ground_pos[1] + 340)
         self.birds.append(bird)
         self.add_widget(bird)
 
@@ -223,7 +190,7 @@ class RunDinoGame(Widget):
         while last_obstacle is not None and cactus.x - last_obstacle.x < min_distance_between_obstacles:
             cactus.x += 10
 
-        cactus.pos = (self.width + random.randint(50, 150), self.ground_pos[1] + 210)
+        cactus.pos = (self.width + random.randint(50, 150), self.ground_pos[1] + 270)
         self.cacti.append(cactus)
         self.add_widget(cactus)
 
@@ -233,14 +200,18 @@ class RunDinoGame(Widget):
         self.ground_pos[0] -= self.ground_speed
         if self.ground_pos[0] <= -self.width:
             self.ground_pos[0] = 0
-        self.dino.move()
+
+        # Atualiza o dinossauro e a animação apenas se não estiver pulando
+        if self.dino.velocity_y == 0:
+            self.dino.move()
+
+        # Atualiza as nuvens
         for cloud in self.clouds:
             cloud.move()
+
         # Atualiza os pássaros
         for bird in self.birds:
             bird.move()
-
-            # Remove pássaros que saíram da tela
             if bird.x < -bird.width:
                 self.birds.remove(bird)
                 self.remove_widget(bird)
@@ -248,20 +219,24 @@ class RunDinoGame(Widget):
         # Atualiza os cactos
         for cactus in self.cacti:
             cactus.move()
-
-            # Remove cactos que saíram da tela
             if cactus.x < -cactus.width:
                 self.cacti.remove(cactus)
                 self.remove_widget(cactus)
 
-        # Adicione aqui a lógica para garantir a distância entre os obstáculos
         self.adjust_obstacle_distance(self.cacti, dt)
         self.adjust_obstacle_distance(self.birds, dt)
-
-    def change_dino_image(self, dt):
     
+    
+    def change_dino_image(self, dt):
         self.ids.dino_image.source = self.dino_image_paths[self.current_dino_index]
         self.current_dino_index = (self.current_dino_index + 1) % len(self.dino_image_paths)
+    
+    def dino_jump(self):
+        if self.dino.y == self.dino.ground_pos:
+            # Animação para o salto do dinossauro
+            jump_animation = Animation(y=self.dino.ground_pos + 100, duration=0.5) + Animation(y=self.dino.ground_pos, duration=0.5)
+            jump_animation.start(self.dino)
+            self.dino.velocity_y = JUMP_VEL
     
 
 # Classe do Aplicativo
@@ -273,3 +248,5 @@ class RunDinoApp(App):
 
 if __name__ == '__main__':
     RunDinoApp().run()
+ 
+ 
